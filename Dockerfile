b49ef7b0ed4eac2fc199efbc34844e997d6ccaa3
NiiWiiCamo <mail@nilsblume.de>
2023-09-23 22:17:10 +0200

    1.0.1

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,17 @@
 FROM python:slim-bullseye
 
 RUN apt-get update && \
-    install -y ffmpeg && \
+    apt-get install -y ffmpeg && \
     apt-get clean
 
 ENV FORMAT_IN="mp4"
 ENV FORMAT_OUT="mp3"
 ENV WATCHDIR="/watch"
 ENV OUTPUTDIR="/output"
+ENV WATCH_SECONDS="300"
+
+ENV PYTHONUNBUFFERED=1
+ENV PYTHONIOENCODING=UTF-8
 
 COPY app /app
 RUN chmod -R +x /app
--- app/convert.py
+++ app/convert.py
@@ -1,11 +1,14 @@
 import os
 import subprocess
 import sys
+from time import sleep
 
 watchdir = os.environ['WATCHDIR']
 outputdir = os.environ['OUTPUTDIR']
 extension_in = os.environ['FORMAT_IN']
 extension_out = os.environ['FORMAT_OUT']
+sleep_duration = int(os.environ['WATCH_SECONDS'])
+
 
 def findFiles(name=extension_in, path=watchdir):
   result = []
@@ -16,41 +19,49 @@ def findFiles(name=extension_in, path=watchdir):
         result.append(os.path.join(root, file))
   return result
 
-# def convert(file, targetformat=extension_out):
-#   file_name, file_extension = os.path.splitext(file)
-#   file_output = file_name + "." + targetformat
-#   print(file)
-#   print(file_output)
-#   stream = ffmpeg.input(file)
-#   stream = ffmpeg.output("/watch/test.mp3")
-#   # stream = ffmpeg.output(file_output)
-#   ffmpeg.run(stream)
 
 def convert(file, targetformat=extension_out):
   file_name, file_extension = os.path.splitext(file)
   file_output = file_name + "." + targetformat
-  print(file)
-  print(file_output)
+  print("converting", file, "to format", targetformat)
   subprocess.call([
     'ffmpeg',
-    '-i',
     '-vn',
     '-nostdin',
     '-hide_banner',
     '-nostats',
     '-loglevel',
     'panic',
-    '-y', 
+    '-y',
+    '-i',
     file,
     file_output
   ])
+  print("success")
+
+
+def filehandler():
+  # find files with extension
+  for file in findFiles(extension_in, watchdir):
+    # convert file
+    convert(file)
+    # remove source file
+    os.remove(file)
+    
+    # move file or parent directory to target dir
+    if ( not os.path.dirname(file) == watchdir ):
+      subprocess.call(['mv', os.path.dirname(file), outputdir])
+    else:
+      subprocess.call(['mv', file, outputdir])
 
 
 def main():
-  # find files with extension
-  for file in findFiles(extension_in, watchdir):
-    convert(file)
-
+  print("Trying to convert", extension_in, "files to", extension_out)
+  print("Watching directory", watchdir, "with a", sleep_duration, "second delay between scans")
+  while True:
+    filehandler()
+    sleep(sleep_duration)
+    
 
 if __name__ == '__main__':
   sys.exit(main())
