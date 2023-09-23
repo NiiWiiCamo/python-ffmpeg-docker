0fa2a20b060541b4d6c356c6f31d4b2fe9f7008f
NiiWiiCamo <mail@nilsblume.de>
2023-09-23 23:04:29 +0200

    1.0.2

--- app/convert.py
+++ app/convert.py
@@ -9,14 +9,19 @@ extension_in = os.environ['FORMAT_IN']
 extension_out = os.environ['FORMAT_OUT']
 sleep_duration = int(os.environ['WATCH_SECONDS'])
 
+image_formats = [ "jpg", "jpeg", "png", "tbn", "gif" ]
 
-def findFiles(name=extension_in, path=watchdir):
+if isinstance(extension_in, str):
+  extension_in = [ extension_in ]
+
+def findFiles(patterns: list = extension_in, path: str = watchdir):
   result = []
   for root, dirs, files in os.walk(path):
     for file in files:
       file_name, file_extension = os.path.splitext(file)
-      if name in file_extension:
-        result.append(os.path.join(root, file))
+      for pattern in patterns:
+        if pattern in file_extension:
+          result.append(os.path.join(root, file))
   return result
 
 
@@ -48,6 +53,8 @@ def filehandler():
     # remove source file
     os.remove(file)
     
+    posterhandler(os.path.dirname(file))
+
     # move file or parent directory to target dir
     if ( not os.path.dirname(file) == watchdir ):
       subprocess.call(['mv', os.path.dirname(file), outputdir])
@@ -55,11 +62,19 @@ def filehandler():
       subprocess.call(['mv', file, outputdir])
 
 
+def posterhandler(dir):
+  for file in findFiles(image_formats, dir):
+    file_name, file_extension = os.path.splitext(file)
+    new_file = os.path.join(os.path.dirname(file), "poster" + file_extension )
+    subprocess.call(['mv', file, new_file])
+
+
 def main():
   print("Trying to convert", extension_in, "files to", extension_out)
   print("Watching directory", watchdir, "with a", sleep_duration, "second delay between scans")
   while True:
     filehandler()
+    print("wating", sleep_duration, "seconds...")
     sleep(sleep_duration)
     
 
